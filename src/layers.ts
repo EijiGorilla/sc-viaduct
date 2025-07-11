78f3a4f7f89136975069ae3beb0772fafe42de4d
EijiGorilla <emasuz@hotmail.com>
2025-07-11 09:10:55 +0800

    updated

--- src/layers.ts
+++ src/layers.ts
@@ -13,7 +13,6 @@ import {
   TextSymbol3DLayer,
 } from "@arcgis/core/symbols";
 import SolidEdges3D from "@arcgis/core/symbols/edges/SolidEdges3D";
-import { labelSymbol3DLine } from "./Label";
 
 /* Standalone table for Dates */
 export const dateTable = new FeatureLayer({
@@ -137,20 +136,37 @@ export const rowLayer = new FeatureLayer({
 });
 
 // * Station Layer * //
-const stationLayerTextSymbol = labelSymbol3DLine({
-  materialColor: "#d4ff33",
-  fontSize: 15,
-  fontFamily: "Ubuntu Mono",
-  fontWeight: "normal",
-  haloColor: "black",
-  haloSize: 0.5,
-  vOffsetScreenLength: 100,
-  vOffsetMaxWorldLength: 700,
-  vOffsetMinWorldLength: 80,
-});
-
-var labelClass = new LabelClass({
-  symbol: stationLayerTextSymbol,
+const stationLayerTextSymbol = new LabelClass({
+  symbol: new LabelSymbol3D({
+    symbolLayers: [
+      new TextSymbol3DLayer({
+        material: {
+          color: "#d4ff33",
+        },
+        size: 13,
+        halo: {
+          color: "black",
+          size: 0.5,
+        },
+        font: {
+          family: "Ubuntu Mono",
+        },
+      }),
+    ],
+    verticalOffset: {
+      screenLength: 100,
+      maxWorldLength: 150,
+      minWorldLength: 120,
+    },
+    callout: {
+      type: "line", // autocasts as new LineCallout3D()
+      color: "white",
+      size: 0.7,
+      border: {
+        color: "grey",
+      },
+    },
+  }),
   labelPlacement: "above-center",
   labelExpressionInfo: {
     expression: 'DefaultValue($feature.Station, "no data")',
@@ -167,7 +183,7 @@ export const stationLayer = new FeatureLayer({
   },
   layerId: 6,
   title: "Station",
-  labelingInfo: [labelClass],
+  labelingInfo: [stationLayerTextSymbol],
   elevationInfo: {
     mode: "relative-to-ground",
   },
